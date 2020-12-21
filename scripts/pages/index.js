9bfbb87830799056caa60c0ad087b97893b3d4c4
Binatik <47430210+Binatik@users.noreply.github.com>
2020-12-21 23:38:03 +0300

    add Test Event

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,8 +2,18 @@ import { musicInit } from '../media/music.js';
 import { radioInit } from '../media/radio.js';
 import { videoInit } from '../media/video.js';
 
-const tabBtnMedia = document.querySelectorAll('.player__btn');
-tabBtnMedia.forEach((tab) => {});
+const tabBtnMedia = document.querySelectorAll('.tab__btn');
+const mediaTab = document.querySelectorAll('.media');
+
+tabBtnMedia.forEach((tab, i) => tab.addEventListener('click', (e) =>
+    (
+        clickBtn(e, tab, i)))
+    );
+
+function clickBtn(event, tab, index){
+    console.log(mediaTab[index]);
+}
+
 
 musicInit();
 radioInit();
