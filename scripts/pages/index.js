0ef7659c1dc050a400488530e6c3128d421741ba
Binatik <47430210+Binatik@users.noreply.github.com>
2020-12-21 23:17:15 +0300

    adjustment media

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,6 +2,10 @@ import { musicInit } from '../media/music.js';
 import { radioInit } from '../media/radio.js';
 import { videoInit } from '../media/video.js';
 
+const tabBtnMedia = document.querySelectorAll('.player__btn');
+tabBtnMedia.forEach((tab) => {});
+
 musicInit();
 radioInit();
 videoInit();
+
